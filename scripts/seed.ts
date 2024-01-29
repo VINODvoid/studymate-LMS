3ac3e39012c1eb8f7640d7b5db3ea0db213c90a5
VINODvoid <iamlearner.v@gmail.com>
2024-01-29 06:57:56 +0530

    Add new dependencies and components

---
+++ scripts/seed.ts
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+
+const database = new PrismaClient();
+async function main(){
+    try {
+        await database.category.createMany({
+            data:[
+                { name:"Computer Science"},
+                { name:"Music"},
+                { name:"Fitness"},
+                { name:"Photography"},
+                { name:"Accounting"},
+                { name:"Engineering"},
+                { name:"Arts & Crafts "},
+                { name:"Business"},
+            ]
+    });
+    console.log("Success!");
+    
+
+    } catch (error) {
+        console.log("Error Seeding the database Categories",error);
+    }
+    finally{
+        await database.$disconnect();
+    }
+}
+
+main();
